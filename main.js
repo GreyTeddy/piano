7fdc95cf2458ce9275bc44f29bf51e656d77256d
Dennis Duka <dukadennis7@gmail.com>
2022-10-25 02:48:53 +0100

    html selectors and small changes

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 //constants
 var osc;
 var keyWidth;
-var numberOfKeys = 30;
-var sel;
+var pianoWidth;
+var pianoHeight
+var numberOfKeys;
+var canvas;
 
 function getOsc(waveType){
 	osc = new p5.Oscillator();
 	osc.setType(waveType);
-	osc.amp(1);
+	osc.amp(0.5);
 	return osc;
 }
 
@@ -23,26 +25,24 @@ function getBlackKeys(notes){
 }
 
 function playWithTouch(){
-	osc.freq(440*(2**(1/12))**Math.floor(map(mouseX,0,windowWidth,3,numberOfKeys+3)));
+	osc.freq(440*(2**(1/12))**Math.floor(map(mouseX,0,pianoWidth,3,numberOfKeys+3)));
 }
 
 function setup(){
-	sel = createSelect();
-	sel.position(0,410);
-	sel.option("sine");
-	sel.option("triangle");
-	sel.option("square");
-	sel.option("sawtooth");
-	sel.changed(getWave);
+	pianoWidth = window.innerWidth;
+	pianoHeight = window.innerHeight*0.6;
+	keyWidth = 50;
+	numberOfKeys = windowWidth/keyWidth;
 	keyWidth = windowWidth/numberOfKeys;
 	blackKeys = getBlackKeys(numberOfKeys);
-	createCanvas(windowWidth,windowHeight);
+	canvas = createCanvas(pianoWidth,pianoHeight);
+	canvas.parent('piano')
 	for(var i=0;i<numberOfKeys*2;i++){
 		fill(255);
 		if(blackKeys.indexOf(i) > -1){
 			fill(0);
 		}
-		rect(i*keyWidth,0,keyWidth,400);
+		rect(i*keyWidth,0,keyWidth,pianoHeight-1);
 	}
 	osc = getOsc("sine");
 }
@@ -51,12 +51,17 @@ function draw(){
 	playWithTouch();
 }
 
-function getWave(){
-	var item = sel.value();
-	osc.setType(item);
+function setWave(select){
+	osc.setType(select.value);
 }
+function setAmplitude(slider){
+	console.log(slider.parentElement.children[2].innerHTML = slider.value)
+	osc.amp(Number(slider.value)/100);
+}
+
+
 function mousePressed(){
-	if(mouseY<400){
+	if(mouseY<pianoHeight){
 		osc.start();
 	}
 }
